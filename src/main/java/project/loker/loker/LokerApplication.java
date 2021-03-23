e96807b0280879abe6e48768789a6306315c06ec
peje86 <piejay86@gmail.com>
2021-03-23 21:34:34 +0700

    deploy dev loker

--- src/main/java/project/loker/loker/LokerApplication.java
+++ src/main/java/project/loker/loker/LokerApplication.java
@@ -1,17 +1,51 @@
+// package project.loker.loker;
+
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// @SpringBootApplication
+// public class LokerApplication {
+
+
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(LokerApplication.class, args);
+// 	}
+
+// }
+
+
 package project.loker.loker;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-@SpringBootApplication
-public class LokerApplication {
+import org.springframework.jdbc.core.JdbcTemplate;
 
 
 
+@SpringBootApplication  
+public class LokerApplication implements CommandLineRunner {
+
+    @Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LokerApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+	    String sql = "INSERT INTO roles (name) VALUES ("
+	            + "'ROLE_ADMIN')";
+		 
+	    int rows = jdbcTemplate.update(sql);
+	    if (rows > 0) {
+	        System.out.println("A new role has been inserted.");
+	    }
 	}
 
 }
